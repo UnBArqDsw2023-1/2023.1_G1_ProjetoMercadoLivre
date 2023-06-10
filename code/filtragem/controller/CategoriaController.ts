66862d33258c6d25462d9cd3d1e58cb8c4005b6b
Julio Cesar <julio0033@hotmail.com>
2023-06-10 19:24:35 -0300

    Adicionando implementação da CategoriaController
    Co-authored-by: Bruna Lima <lbruna886@gmail.com>

--- code/filtragem/controller/CategoriaController.ts
+++ code/filtragem/controller/CategoriaController.ts
@@ -14,6 +14,34 @@ export class CategoriaController {
         return "Cadastrado com sucesso!";
     }
 
+    atualizar(categoria: Categoria): string {
+        const indexCategoria = this.dataBase.categorias.findIndex((ctg) => ctg.getId() == categoria.getId())
+        if(indexCategoria >= 0) {
+            this.dataBase.categorias[indexCategoria] = categoria;
+            return "Categoria atualizada com sucesso!"
+        }
+        return "Esta categoria não existe!";
+    }
+
+    buscarPorID(id: number): Categoria | string {
+        const categoriaEncontrada = this.dataBase.categorias.find((categoria) => categoria.getId() == id);
+        return categoriaEncontrada ? categoriaEncontrada : "Esta categoria não existe!" 
+    }
+
+    buscarPorNome(nome: string): Categoria | string {
+        const categoriaEncontrada = this.dataBase.categorias.find((categoria) => categoria.getNome() == nome);
+        return categoriaEncontrada ? categoriaEncontrada : "Esta categoria não existe!"
+    }
+
+    remover(id: number): string {
+        const indexCategoria = this.dataBase.categorias.findIndex((categoria) => categoria.getId() == id)
+        if(indexCategoria >= 0){
+            this.dataBase.categorias.splice(indexCategoria, 1);
+            return "Categoria removida com sucesso!";
+        }
+        return "Esta categoria não existe!";
+    }
+
     obterTodos(): void {
         this.dataBase.categorias.forEach((categoria) => {
             console.log(categoria.toString());
